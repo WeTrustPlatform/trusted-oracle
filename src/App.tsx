ce379d661f5d149c5320598c8a973fac2fe62e2c
Vu Hoang Minh <vuhminh@gmail.com>
2019-09-17 14:29:22 +0700

    fix: try with basename

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ const NotificationsDialog = (
 
 export const App = () => {
   return (
-    <HashRouter>
+    <HashRouter basename="/">
       <ThemeProvider
         value={{
           colors: {
