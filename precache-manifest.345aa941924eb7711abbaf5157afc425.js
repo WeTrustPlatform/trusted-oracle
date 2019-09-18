b7deb76adb2ba69ffc7dd89ce5edb22573f818d8
Vu Hoang Minh <vuhminh@gmail.com>
2019-09-18 16:39:28 +0700

    Updates

---
+++ precache-manifest.345aa941924eb7711abbaf5157afc425.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b277851eaf7d9f136b53cee7afce73bc",
+    "url": "/trusted-oracle/index.html"
+  },
+  {
+    "revision": "08dca11ccb229f87d816",
+    "url": "/trusted-oracle/static/css/main.771e67f9.chunk.css"
+  },
+  {
+    "revision": "43d2e210beba0e605722",
+    "url": "/trusted-oracle/static/js/2.01672ac4.chunk.js"
+  },
+  {
+    "revision": "08dca11ccb229f87d816",
+    "url": "/trusted-oracle/static/js/main.75c7630c.chunk.js"
+  },
+  {
+    "revision": "24dec507ecc39d4b560e",
+    "url": "/trusted-oracle/static/js/runtime~main.c82802da.js"
+  },
+  {
+    "revision": "3ac95a3593201f1d7f1ecb65217eb18e",
+    "url": "/trusted-oracle/static/media/card-posted.3ac95a35.svg"
+  },
+  {
+    "revision": "537b4cb2bb8afdbf142e6a5c53062206",
+    "url": "/trusted-oracle/static/media/card-trst.537b4cb2.svg"
+  },
+  {
+    "revision": "564b9965a073c7c706161e29e9106e50",
+    "url": "/trusted-oracle/static/media/how-it-works-mobile.564b9965.svg"
+  },
+  {
+    "revision": "c8a63f5c0de85715a446ebb7aad8e6ab",
+    "url": "/trusted-oracle/static/media/how-it-works.c8a63f5c.png"
+  },
+  {
+    "revision": "b23ff1cfb6724c0396b301d5001adfbf",
+    "url": "/trusted-oracle/static/media/logo-mobile.b23ff1cf.svg"
+  },
+  {
+    "revision": "e7e7ac833a0b0b1d92b7083e0182217c",
+    "url": "/trusted-oracle/static/media/logo.e7e7ac83.svg"
+  },
+  {
+    "revision": "ba23639b59c342b238d60b6e6f4295b9",
+    "url": "/trusted-oracle/static/media/metamask-account-icon.ba23639b.svg"
+  },
+  {
+    "revision": "7f2daadb9728453b75389a60aba3e05a",
+    "url": "/trusted-oracle/static/media/oracle-hero.7f2daadb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/trusted-oracle/precache-manifest.9e53f81145fbf43f33e4c61b4d67fd8d.js"
+  "/trusted-oracle/precache-manifest.345aa941924eb7711abbaf5157afc425.js"
 );
 
 self.addEventListener('message', (event) => {
