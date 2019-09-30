26e70183e30a0e2ad20fffda8073f4f8b9906e7b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-30 15:57:51 +0000

    Deploy WeTrustPlatform/trusted-oracle to github.com/WeTrustPlatform/trusted-oracle.git:gh-pages

---
+++ precache-manifest.4150dc26c284169a5e8e9f1c36eaacda.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "937ecda37d172b3edb475ef58f346583",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cbdc4ab8a0e5968f843",
+    "url": "/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "9e97400242b1476007b4",
+    "url": "/static/css/main.77a16150.chunk.css"
+  },
+  {
+    "revision": "1cbdc4ab8a0e5968f843",
+    "url": "/static/js/2.6d5a6ebd.chunk.js"
+  },
+  {
+    "revision": "9e97400242b1476007b4",
+    "url": "/static/js/main.de0663b2.chunk.js"
+  },
+  {
+    "revision": "404ec65cc01d73f68eb3",
+    "url": "/static/js/runtime~main.7159b203.js"
+  },
+  {
+    "revision": "3ac95a3593201f1d7f1ecb65217eb18e",
+    "url": "/static/media/card-posted.3ac95a35.svg"
+  },
+  {
+    "revision": "537b4cb2bb8afdbf142e6a5c53062206",
+    "url": "/static/media/card-trst.537b4cb2.svg"
+  },
+  {
+    "revision": "564b9965a073c7c706161e29e9106e50",
+    "url": "/static/media/how-it-works-mobile.564b9965.svg"
+  },
+  {
+    "revision": "c8a63f5c0de85715a446ebb7aad8e6ab",
+    "url": "/static/media/how-it-works.c8a63f5c.png"
+  },
+  {
+    "revision": "b23ff1cfb6724c0396b301d5001adfbf",
+    "url": "/static/media/logo-mobile.b23ff1cf.svg"
+  },
+  {
+    "revision": "e7e7ac833a0b0b1d92b7083e0182217c",
+    "url": "/static/media/logo.e7e7ac83.svg"
+  },
+  {
+    "revision": "ba23639b59c342b238d60b6e6f4295b9",
+    "url": "/static/media/metamask-account-icon.ba23639b.svg"
+  },
+  {
+    "revision": "7f2daadb9728453b75389a60aba3e05a",
+    "url": "/static/media/oracle-hero.7f2daadb.png"
+  }
+]);
